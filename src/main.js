09c5d54553f9962ae0d10bd9871d9f57ab710154
Niamh <niamh.linguist@gmail.com>
2025-06-04 14:15:39 +0300

    loading assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-import kaplay from 'kaplay';
+import k from './kaplayCtx';
 
-kaplay();
+k.loadSprite('chemical-bg', 'graphics/chemical-bg.png');
+k.loadSprite('platforms', 'graphics/platforms.png');
+k.loadSprite('sonic', 'graphics/sonic.png', {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite('rings', 'graphics/ring.png', {
+  sliceX: 16,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 15, loop: true, speed: 30 },
+  },
+});
+k.loadSprite('motobug', 'graphics/motobug.png', {
+  sliceX: 5,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
+k.loadFont('mania', 'fonts/mania.ttf');
+k.loadSound('destroy', 'sounds/destroy.wav');
+k.loadSound('jump', 'sounds/jump.wav');
+k.loadSound('ring', 'sounds/ring.wav');
+k.loadSound('city', 'sounds/city.wav');
+k.loadSound('hyper-ring', 'sounds/hyperring.wav');
+k.loadSound('hurt', 'sounds/hurt.wav');
